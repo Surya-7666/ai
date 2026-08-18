594cb6b12e0a791e33691cf506c30a95304cd3b7
jones.j <jones.j8637@gmail.com>
2026-08-18 21:50:11 +0530

    Update scalekit.ts

--- src/lib/scalekit.ts
+++ src/lib/scalekit.ts
@@ -6,12 +6,3 @@ export const scalekit = new Scalekit(
   process.env.SCALEKIT_CLIENT_ID!,
   process.env.SCALEKIT_CLIENT_SECRET!,
 );
-
-// import { Scalekit } from "@scalekit-sdk/node"; // test
-
-// const environmentUrl = "https://barigda5jx2f2.scalekit.dev";
-// const clientId = "skc_134853420036129042";
-// const clientSecret =
-//   "test_8zCA0PhJ8x0PGlzFkLYYrJcTXJQYrOzmDO2LggTKLjlZ2cRBcOMaoxnfeKzcVP0L";
-
-// export const scalekit = new Scalekit(environmentUrl, clientId, clientSecret);
