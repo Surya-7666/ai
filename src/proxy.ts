7ff75ebc494d2def4c57180ca9b8092b3581b6c9
jones.j <jones.j8637@gmail.com>
2026-08-18 21:49:43 +0530

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/getSession";
 
 export async function proxy(req: NextRequest) {
-  const AppUrl = "http://localhost:3000";
   const session = await getSession();
   if (!session) {
-    // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
-    return NextResponse.redirect(`${AppUrl}`);
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
   }
   return NextResponse.next();
 }
